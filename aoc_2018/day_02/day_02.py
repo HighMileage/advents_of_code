0efd6e97f29a07edee7403d6076519a1b3de208f
HighMileage <rcsellers@gmail.com>
2018-12-02 22:45:56 -0600

    Day 2 part 2

--- aoc_2018/day_02/day_02.py
+++ aoc_2018/day_02/day_02.py
@@ -13,12 +13,42 @@ def repeats_characters(string, appearances):
     id_list.sort()
     return appearances in [len(list(group)) for _, group in groupby(id_list)]
 
-def main():
+
+def checksum():
     two_repeats = sum([repeats_characters(box_id, 2) for box_id in inputs()])
     three_repeats = sum([repeats_characters(box_id, 3) for box_id in inputs()])
 
     return two_repeats * three_repeats
 
+
+def one_character_diff(target_id, base_id):
+    return 1 == sum(target != base for target, base in zip(list(target_id), list(base_id)))
+
+
+def compare_to_all_ids(candidate_id):
+    for target_id in inputs():
+        if one_character_diff(target_id, candidate_id):
+            print('Found nearly matching ids:\n    {}\n    {}'.format(target_id, candidate_id))
+            return (target_id, candidate_id)
+    return None
+
+def identify_correct_boxes():
+    near_matches = None
+    for candidate_id in inputs():
+        near_matches = compare_to_all_ids(candidate_id)
+        if near_matches:
+            return near_matches
+
+    return near_matches
+
+
 if __name__ == '__main__':
-    checksum = main()
+    checksum = checksum()
+    print("---------------------------PART 1---------------------------")
     print("Looks like your checksum is {}".format(checksum))
+
+    print("\n---------------------------PART 2---------------------------")
+    box_one_ids, box_two_ids = identify_correct_boxes()
+
+    common_letters = ''.join(char_1 for char_1, char_2 in zip(box_one_ids, box_two_ids) if char_1 == char_2)
+    print('Found these common charactes: {}'.format(common_letters))
