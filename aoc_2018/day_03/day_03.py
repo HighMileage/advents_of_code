111685c59c0114e2cb7d7ba6ec5979739fbf88b6
HighMileage <rcsellers@gmail.com>
2018-12-03 00:41:32 -0600

    Get day 3 part 2 working

--- aoc_2018/day_03/day_03.py
+++ aoc_2018/day_03/day_03.py
@@ -8,17 +8,20 @@ def inputs():
 
 
 def _decode_row(row):
-    start_point, dim_string = row.split('@')[1].strip().split(':')
+    claim_num = row.split('@')[0][1:].strip()
+    _start_point, dim_string = row.split('@')[1].strip().split(':')
+    start_point = tuple(int(point) for point in _start_point.split(','))
     x, y = dim_string.split('x')
-    return (start_point, (int(x), int(y)))
+    return (claim_num, start_point, (int(x), int(y)))
 
 
 def get_impacted_points(claim):
     coordinates = []
 
-    init_x = int(claim[0].split(',')[0])
-    init_y = int(claim[0].split(',')[1])
-    x_range, y_range = claim[1]
+    _claim_num, start_point, x_y_range = claim
+    init_x = int(start_point[0])
+    init_y = int(start_point[1])
+    x_range, y_range = x_y_range
 
     for x_delta in range(1, x_range + 1):
         for y_delta in range(1, y_range + 1):
@@ -26,16 +29,24 @@ def get_impacted_points(claim):
 
     return coordinates
 
-def count_doubles():
+
+def main():
     all_coordinates = []
     for claim in inputs():
         all_coordinates += get_impacted_points(claim)
 
     all_coordinates.sort()
-    print('Here are the doubles')
     number_of_doubles = sum(len(list(group)) >= 2 for coord, group in groupby(all_coordinates))
-    print(number_of_doubles)
+    print('Looks like there are at least {} spots with overlap of 2 or more claims'.format(number_of_doubles))
+
+    singletons = set(coord for coord, group in groupby(all_coordinates) if len(list(group)) == 1)
+
+    for claim in inputs():
+        claim_set = set(get_impacted_points(claim))
+
+        if singletons.intersection(claim_set) == claim_set:
+            print('Found a claim that has no overlap: Claim #{}'.format(claim[0]))
 
 
 if __name__ == '__main__':
-    count_doubles()
+    main()
