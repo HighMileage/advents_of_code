f1e96bd26be1ad5dd7508204748b30ecbbccb8bc
HighMileage <rcsellers@gmail.com>
2018-12-02 20:07:16 -0600

    Add day 1 part 2

--- aoc_2018/day_01/day_01.py
+++ aoc_2018/day_01/day_01.py
@@ -1,5 +1,22 @@
 import csv
 
-with open('input_01.txt', 'r') as input_file:
-    output = sum(int(value[0]) for value in csv.reader(input_file))
-print(output)
+def inputs():
+    with open('input_01.txt', 'r') as input_file:
+        for value in csv.reader(input_file):
+            yield int(value[0])
+
+
+def main():
+    observed_states = set()
+    frequency = 0
+
+    while True:
+        for change in inputs():
+            observed_states.add(frequency)
+            frequency += change
+            if frequency in observed_states:
+                print('Duplicate frequency found at {}'.format(frequency))
+                return
+
+if __name__ == '__main__':
+    main()
