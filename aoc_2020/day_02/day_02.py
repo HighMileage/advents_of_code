837cb98716c6564c6b7c896c8448724539ad6d2c
HighMileage <rcsellers@gmail.com>
2020-12-01 23:49:28 -0600

    Refine day2 implementation and start enforcing fixer

--- aoc_2020/day_02/day_02.py
+++ aoc_2020/day_02/day_02.py
@@ -1,35 +1,40 @@
 import csv
 import sys
 
+
 def inputs(file_path):
     with open(file_path, "r") as input_file:
         for value in csv.reader(input_file):
             yield value[0]
 
+
 def main(file_path):
-    good = []
-    for x in inputs(file_path):
-        range_text, limit_letter, password = x.split(" ")
-        limit_letter = limit_letter.replace(':', '')
+    valid_passwords_part_1 = []
+    valid_passwords_part_2 = []
+
+    for line in inputs(file_path):
+        range_text, limit_letter, password = line.split(" ")
+        limit_letter = limit_letter.replace(":", "")
 
         low = int(range_text.split("-")[0])
         high = int(range_text.split("-")[1])
 
-        count = 0
-        if password[low-1] == limit_letter:
-            count=count+1
+        count = sum([1 for letter in password if letter == limit_letter])
+        if count >= low and count <= high:
+            valid_passwords_part_1.append(password)
 
-        if password[high-1] == limit_letter:
-            count=count+1
+        low_index_present = password[low - 1] == limit_letter
+        high_index_present = password[high - 1] == limit_letter
+        letters_present = [low_index_present, high_index_present]
 
-        if count == 1:
-            good.append(password)
+        if not all(letters_present) and any(letters_present):
+            valid_passwords_part_2.append(password)
 
-        print(limit_letter, high, low, "-----", x)
+    print(f"Found {len(valid_passwords_part_1)} passwords for part 1")
+    print(f"Found {len(valid_passwords_part_2)} passwords for part 2")
 
-    print(len(good))
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
-    print("Day 2: XXXXX --------------------- \n")
+    print("Day 2: Password Philosophy --------------------- \n")
     main(input_file)
