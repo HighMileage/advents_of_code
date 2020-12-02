04afe8beb2d9f55f5a58a3d21177ecfb3b0ff905
HighMileage <rcsellers@gmail.com>
2020-12-01 23:08:54 -0600

    Rough pass on Day 2 part 1

--- aoc_2020/day_02/day_02.py
+++ aoc_2020/day_02/day_02.py
@@ -4,12 +4,29 @@ import sys
 def inputs(file_path):
     with open(file_path, "r") as input_file:
         for value in csv.reader(input_file):
-            yield int(value[0])
+            yield value[0]
 
 def main(file_path):
+    good = []
     for x in inputs(file_path):
-        print(x)
+        range_text, limit_letter, password = x.split(" ")
+        limit_letter = limit_letter.replace(':', '')
 
+        low = int(range_text.split("-")[0])
+        high = int(range_text.split("-")[1])
+
+        count = 0
+        for letter in password:
+            if letter == limit_letter:
+                count=count+1
+                print(f"Got this count {count}")
+
+        if count >= low and count <= high:
+            good.append(password)
+
+        print(limit_letter, high, low, "-----", x)
+
+    print(len(good))
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
