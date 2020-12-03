977f4d0f3d66b664f6d238ffc6c6bdaa4a62f2f3
HighMileage <rcsellers@gmail.com>
2020-12-02 23:18:17 -0600

    Ugh, rough start to part 1

--- aoc_2020/day_03/day_03.py
+++ aoc_2020/day_03/day_03.py
@@ -10,8 +10,21 @@ def inputs(file_path):
 
 def main(file_path):
 
-    for line in inputs(file_path):
-        print(line)
+    x = 0
+    trees = []
+    rows = len(list(inputs(file_path)))
+    expanded_size = rows * 3
+    print(expanded_size)
+    for i, line in enumerate(inputs(file_path)):
+        limit = len(line)
+        multiplier = int(expanded_size * limit)
+        l = line * multiplier
+
+        if l[x] == "#":
+            trees.append(1)
+        x += 3
+
+    print(f"Looks like you'd hit {sum(trees)} trees")
 
 
 if __name__ == "__main__":
