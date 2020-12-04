e0dabd4e87533827be4f3a4cd612d129c679f10e
HighMileage <rcsellers@gmail.com>
2020-12-04 00:00:03 -0600

    Day 4 part i

--- aoc_2020/day_04/day_04.py
+++ aoc_2020/day_04/day_04.py
@@ -4,13 +4,61 @@ import sys
 
 def inputs(file_path):
     with open(file_path, "r") as input_file:
-        for value in csv.reader(input_file):
-            yield value[0]
+        lines = input_file.readlines()
+        return [line.strip() for line in lines]
 
 
 def main(file_path):
-    valid_passwords_part_1 = []
-    valid_passwords_part_2 = []
+    data = list(inputs(file_path))
+    batch = ""
+    entries = []
+    for i, line in enumerate(data):
+        batch = batch + " " + line
+        if i + 1 < len(data) and data[i + 1] == "":
+            entries.append(batch)
+            batch = ""
+        if i + 1 == len(data):
+            entries.append(batch)
+
+    passports = []
+    for entry in entries:
+        attributes = entry.strip().split(" ")
+        meow = {}
+        for i in attributes:
+            k, v = i.split(":")
+            meow[k] = v
+        passports.append(meow)
+
+    valid_passports = [passport for passport in passports if valid(passport)]
+    invalid_passports = [passport for passport in passports if not valid(passport)]
+
+    print(f"Found {len(valid_passports)} valid passports!")
+    print(f"Found {len(invalid_passports)} invalid passports!")
+
+
+def valid(passport):
+    valid_passport_attrs = {
+        "byr",
+        "ecl",
+        "eyr",
+        "hcl",
+        "hgt",
+        "iyr",
+        "pid",
+    }
+
+    valid_northpole_attrs = {
+        "byr",
+        "cid",
+        "ecl",
+        "eyr",
+        "hcl",
+        "hgt",
+        "iyr",
+        "pid",
+    }
+
+    return all(k in passport.keys() for k in valid_passport_attrs)
 
 
 if __name__ == "__main__":
