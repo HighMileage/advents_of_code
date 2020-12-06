09c2a17132b5752128b6035305f97f139b40ab18
HighMileage <rcsellers@gmail.com>
2020-12-06 13:42:24 -0600

    Add Day 5 parts i & ii

--- aoc_2020/day_05/day_05.py
+++ aoc_2020/day_05/day_05.py
@@ -1,5 +1,7 @@
 import csv
 import sys
+import math
+from collections import namedtuple
 
 
 def inputs(file_path):
@@ -9,11 +11,57 @@ def inputs(file_path):
 
 
 def main(file_path):
+    boarding_passes = sorted(seat_information(file_path), key=lambda s: s.id)
+    last_pass = boarding_passes[-1]
+    print(
+        f"Max id from all boarding passes: {last_pass.row} {last_pass.column} ----> {last_pass.id}"
+    )
+
+    for i in range(boarding_passes[0].id, last_pass.id + 1):
+        if i == 0:
+            continue
+        if i not in [boarding_pass.id for boarding_pass in boarding_passes]:
+            print(f"Seat ID: {i} not found")
+            break
+
+
+def seat_information(file_path):
     for line in inputs(file_path):
-        print(line)
+        seat_range = list(range(0, 128))
+        column_range = list(range(0, 8))
+        row = None
+
+        Seat = namedtuple("Seat", ["boarding_pass", "row", "column", "id"])
+        for i, command in enumerate(line):
+            if i < 7:
+                seat_range = pick_partition(seat_range, command)
+            if i == 7:
+                row = seat_range[0]
+            if i >= 7:
+                column_range = pick_partition(column_range, command)
+
+        row = seat_range[0]
+        column = column_range[0]
+        yield Seat(line, row, column, row * 8 + column)
+
+    # print(f"Your seat is Row {row}, Column {column}")
+
+
+def pick_partition(input_range, instruction):
+    front, back = split(input_range)
+    if instruction in ("F", "L"):
+        return front
+    else:
+        return back
+
+
+def split(array):
+    # What to do with odd numbers?
+    half = math.floor(len(array) / 2)
+    return (array[0:half], array[half:])
 
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
-    print("Day 5: XXX --------------------- \n")
+    print("Day 5: Binary Boarding --------------------- \n")
     main(input_file)
