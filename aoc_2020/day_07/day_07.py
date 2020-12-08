ccd854fe0d16982a21a859de390c20931090fd24
HighMileage <rcsellers@gmail.com>
2020-12-08 00:23:43 -0600

    Add Day 7 part ii

--- aoc_2020/day_07/day_07.py
+++ aoc_2020/day_07/day_07.py
@@ -11,16 +11,44 @@ def inputs(file_path):
 def main(file_path):
     data = inputs(file_path)
     bag_rules = get_bag_rules(data)
-    # print(bag_rules)
+    target_bag = "shiny gold"
 
-    good_bags = []
-    for bag in bag_rules.keys():
-        if can_contain_bag(bag, "shiny gold", bag_rules):
-            good_bags.append(bag)
+    good_bags = list(
+        bag for bag in bag_rules.keys() if can_contain_bag(bag, target_bag, bag_rules)
+    )
 
-    print(f"Found {len(good_bags)} bag that will contain 'shiny gold' bags")
+    print(f"Found {len(good_bags)} bags that must contain {target_bag} bag")
     for b in good_bags:
         print("\t", b)
+
+    total_bag_count = get_bag_count(target_bag, bag_rules)
+    print(
+        f"Total bag count within a shiny gold is {total_bag_count - 1}"
+    )  # Minus one for outer most bag
+
+
+def get_bag_count(bag_type, rules):
+
+    eventually_singleton_bags = [bag_type]
+    bags_with_contents = []
+
+    while True:
+        new_bags = []
+        for bag in eventually_singleton_bags:
+            if len(rules[bag]) == 0:
+                new_bags.append(bag)
+                continue
+
+            bags_with_contents.append(bag)
+            for b, count in rules[bag].items():
+                for _ in range(0, int(count)):
+                    new_bags.append(b)
+
+        if new_bags == eventually_singleton_bags:
+            break
+        eventually_singleton_bags = new_bags
+
+    return len(eventually_singleton_bags + bags_with_contents)
 
 
 def can_contain_bag(outer_bag, target_bag, rules):
