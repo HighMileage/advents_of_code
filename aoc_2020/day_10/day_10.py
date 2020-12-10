1e15796df6b97d0cf7053918b1478c7c22265290
HighMileage <rcsellers@gmail.com>
2020-12-10 09:16:39 -0600

    Fix typos in comment

--- aoc_2020/day_10/day_10.py
+++ aoc_2020/day_10/day_10.py
@@ -73,7 +73,7 @@ def main(data):
 
     # It was easiest for me to think about how removing adapaters "condensed"
     # or spread joltage difference to a nearby neighbor. This joltage
-    # difference condesning was only possible for consecutive neighbors that
+    # difference condensing was only possible for consecutive neighbors that
     # were 1/2 jolts away from each other. Imagine the joltage adapaters below
     # and their corresponding joltage diffs:
     #     Joltages:        0, 1, 4, 5, 6, 7, 10
@@ -108,11 +108,11 @@ def main(data):
     #
     # 1 1 1 1 --> Starting point
     # 1  2  1 --> Condense from middle
-    #  2  1 1 --> Condense from right
-    #     3 1 --> Condense from right moar
-    # 1 1  2
-    # 1 3
-    #  2   2
+    #  2  1 1 --> Condense from left
+    #     3 1 --> Condense from left moar
+    # 1 1  2  --> Condense from right
+    # 1 3     --> Condense from right moar
+    #  2   2  --> Condense from side centers
 
     total = 1
     run_type_combinations = {"11": 2, "111": 4, "1111": 7}
